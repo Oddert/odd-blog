ca54d0213c854b7a6e27963cdb50f0af3a722122
Robyn <oddert@gmail.com>
2019-01-23 22:10:25 +0000

    image inputs support multiple dom elems

--- public/js/master.js
+++ public/js/master.js
@@ -10,7 +10,7 @@ function handleNewInput (e) {
   let newElem
   switch(e.target.name) {
     case 'new_paragraph':
-      let textAreas = form.querySelectorAll('text_input')
+      let textAreas = form.querySelectorAll('.text_input')
       newElem = document.createElement('DIV')
       newElem.className = 'text_input'
 
@@ -23,9 +23,13 @@ function handleNewInput (e) {
       break;
     case 'new_image':
       let images = form.querySelectorAll('.image_input')
-      newElem = document.createElement('input')
-      newElem.name = `image[${images.length}]`
+      newElem = document.createElement('DIV')
       newElem.className = 'image_input'
+
+      let newImageSrcInput = document.createElement('input')
+      newImageSrcInput.name = `image[${images.length}]`
+      newImageSrcInput.className = 'image_input'
+      newElem.appendChild(newImageSrcInput)
       form.appendChild(newElem)
       break;
     default:
@@ -37,3 +41,25 @@ function handleNewInput (e) {
 newButtons.forEach(each => each.onclick = handleNewInput)
 
 for (let i = 0; i < 5; i++) handleNewInput({ target: { name: 'new_paragraph' } })
+
+function writeName() {
+  let greeting = ['Hello', 'Hi there', 'Welcome', 'Signed in as', 'Director of Project']
+  let name = ['James', 'Erin', 'Felicity', 'Violet', 'Victoria', 'Fiona', 'Heather']
+  const pick = arr => arr[Math.floor(Math.random()*arr.length)]
+  let elem = document.createElement('H1')
+  elem.textContent = `${pick(greeting)} Ms Robyn ${pick(name)} Veitch`
+  elem.style.position = 'fixed'
+  elem.style.top = '0'
+  elem.style.right = '0'
+  elem.style.padding = '10px'
+  elem.style.margin = '0'
+  elem.style.color = '#ecf0f1'
+  elem.style.textShadow = '0px 0px 10px rgba(0,0,0,1)'
+  elem.style.fontFamily = 'helvetica'
+  document.body.append(elem)
+  setTimeout(() => document.body.removeChild(elem), 29000)
+}
+
+writeName()
+
+setInterval(writeName, 30000)
