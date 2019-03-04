eb5ec329a1248abba6a1b586e9a9bcfe278025a5
Robyn <oddert@gmail.com>
2019-03-04 22:51:17 +0000

    added navbar animation, styled the folio icons

--- public/js/folio/index.js
+++ public/js/folio/index.js
@@ -10,6 +10,23 @@ const socialMediaButtonContainer = document.querySelector('.social_media')
 const socialMediaButtons = socialMediaButtonContainer.querySelectorAll('a')
 const linksIndicator = document.querySelector('.links_indicator')
 
+const nav = document.querySelector('nav')
+
+function debounce (func, wait = 20, immediate = true) {
+  let timeout;
+  return function () {
+    let context = this, args = arguments
+    let later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    let callNow = immediate && !timeout
+    clearTimeout (timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+}
+
 function showLinksIndicator (e) {
   linksIndicator.textContent = links[e.target.closest('a').className]
 }
@@ -18,8 +35,32 @@ function hideLinksIndicator () {
   linksIndicator.textContent = ' '
 }
 
+function updateScroll () {
+  const scroll = window.scrollY;
+  const height = window.innerHeight
+  const diff = height - scroll
+  // console.log(scroll, height, diff, ((height - scroll) / height) * 100)
+  if (diff <= 50) {
+    nav.style.height = "50px"
+    nav.classList.add('nav_bar')
+  }
+  else {
+    nav.style.height = `${((height - scroll) / height) * 100}%`;
+    nav.classList.remove('nav_bar')
+  }
+}
+
+function initPage (e) {
+  const logoHidden = document.querySelector('#intro_hidden .content svg')
+  logoHidden.style.opacity = '0'
+}
+
+window.addEventListener('scroll', e => debounce(updateScroll(), 5))
+
 socialMediaButtonContainer.addEventListener('mouseout', hideLinksIndicator)
 
 socialMediaButtons.forEach(each => {
   each.addEventListener('mouseover', showLinksIndicator)
 })
+
+document.addEventListener('DOMContentLoaded', initPage)
