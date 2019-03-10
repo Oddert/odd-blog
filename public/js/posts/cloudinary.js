adb1e6ba8b3f84edcd8cd3d91e00f9403e363b49
Robyn <oddert@gmail.com>
2019-03-10 01:23:29 +0000

    added rendering for all saved images

--- public/js/posts/cloudinary.js
+++ public/js/posts/cloudinary.js
@@ -1,7 +1,51 @@
 const imageControlContainer = document.querySelector('.image_control--container')
 const imageControl = document.querySelector('.image_control')
+const cloudinaryImages = document.querySelector('.cloudinary_images')
+const newImage = document.querySelector('.new_image')
+console.log(newImage)
 
-function toggleImageControl (open) {
-  if (imageControlContainer.classList.contains('open') || open) imageControlContainer.classList.remove('open')
-  else imageControlContainer.classList.add('open')
+const store = {
+  all: [],
+  post: []
 }
+
+function toggleImageControl (close) {
+  if (imageControlContainer.classList.contains('open') || close) {
+    imageControlContainer.classList.remove('open')
+  }
+  else {
+    imageControlContainer.classList.add('open')
+  }
+}
+
+function getImages () {
+  const url = `/api/images/`
+  const options = {
+    method: 'GET'
+  }
+  fetch(url, options)
+    .then(res => res.json())
+    .then(images => {
+      console.log({ images })
+      store.all = [...images]
+      renderAll()
+    })
+    .catch(err => console.log(err))
+}
+
+function renderAll () {
+  console.log(store)
+  let innerHtml = store.all.map(each => `<div>${each.url}</div>`).join('')
+  cloudinaryImages.innerHTML = innerHtml
+}
+
+function addImage (e) {
+  console.log('AAAAAAHHHHHHHHHH')
+  e.preventDefault()
+}
+
+imageControlContainer.addEventListener('click', e => {
+  if (e.target.classList.contains('image_control--container')) toggleImageControl (true)
+})
+newImage.onsubmit = addImage
+document.addEventListener('DOMContentLoaded', getImages)
