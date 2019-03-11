efe13cbb3904649a74564821a298f2cec5364c2f
Robyn <oddert@gmail.com>
2019-03-11 22:48:39 +0000

    styled image icons, added copy to clipboard func, added 'image' button

--- public/js/posts/cloudinary.js
+++ public/js/posts/cloudinary.js
@@ -1,12 +1,38 @@
 const imageControlContainer = document.querySelector('.image_control--container')
-const imageControl = document.querySelector('.image_control')
-const cloudinaryImages = document.querySelector('.cloudinary_images')
-const newImage = document.querySelector('.new_cloudinary_image')
+const imageControl          = document.querySelector('.image_control')
+const cloudinaryImages      = document.querySelector('.cloudinary_images')
+const newImage              = document.querySelector('.new_cloudinary_image')
+const openImageControl      = document.querySelector('.open_image_control')
 console.log(newImage)
 
 const store = {
   all: [],
   current: []
+}
+
+const copy = str => {
+  const shadow_input = document.createElement('textarea')
+  shadow_input.value = str
+  document.body.appendChild(shadow_input)
+  shadow_input.select()
+  document.execCommand("copy")
+  document.body.removeChild(shadow_input)
+  flashNotif(shadow_input.value)
+  console.log('Copied text:', shadow_input.value)
+}
+
+const flashNotif = str => {
+  const notif = document.querySelector('.notifier')
+  notif.hidden = false
+  notif.querySelector('span').textContent = str
+  notif.classList.add('show')
+  setTimeout(() => {
+    notif.classList.add('fade')
+    setTimeout(() => {
+      notif.classList.remove('show', 'fade')
+      setTimeout(() => notif.hidden = true, 300)
+    }, 1000)
+  }, 1000)
 }
 
 function toggleImageControl (close) {
@@ -33,22 +59,40 @@ function getImages () {
     .catch(err => console.log(err))
 }
 
+function copyImageLink (e) {
+  const value = e.target.closest('button').querySelector('textarea').value
+  copy(value)
+}
+
 function renderAll () {
   console.log(store)
-  let innerHtml = store.all.map(each => `<div>${each.url}</div>`).join('')
+  let innerHtml = store.all.map(each => `
+      <button class="image_selection">
+        <div class="image_selection__thumb">
+          <img src="${each.url}" />
+        </div>
+        <div class="image_selection__details">
+          <p class="image_selection__title">${each.width} x ${each.height}</p>
+          <p class="image_selection__url">${each.url}</p>
+          <textarea type="text" value="${each.url}" hidden >${each.url}</textarea>
+        </div>
+      </button>
+    `).join('')
   cloudinaryImages.innerHTML = innerHtml
+  cloudinaryImages
+    .querySelectorAll('.image_selection')
+    .forEach(each => each.onclick = copyImageLink)
 }
 
 function addImage (e) {
   e.preventDefault()
-  const file = e.target.querySelector('input[type=file]').files[0]
-  const sendData = new FormData()
-  console.log({ data_file: sendData.get('file') })
-
-  sendData.append("file", file)
   const url = `/api/images/`
-  console.log({ file, sendData, data_file: sendData.get('file') })
+  const file = e.target.querySelector('input[type=file]').files[0]
 
+  const sendData = new FormData()
+  // console.log({ data_file: sendData.get('file') })
+  sendData.append("file", file)
+  // console.log({ file, sendData, data_file: sendData.get('file') })
   const headers = new Headers()
 
   const options = {
@@ -70,6 +114,7 @@ imageControlContainer.addEventListener('click', e => {
   if (e.target.classList.contains('image_control--container')) toggleImageControl (true)
 })
 newImage.onsubmit = addImage
+openImageControl.onclick = () => toggleImageControl()
 document.addEventListener('DOMContentLoaded', getImages)
 
 
